ccd93cc3053ca87788a19a2b22fc6bdc8bded4d8
TagaibekEdil <Tagaibek.edil@gmail.com>
2023-04-11 21:21:58 +0600

    first comit

---
+++ src/Main.java
@@ -0,0 +1,44 @@
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+
+    static final String DB_URL = "jdbc:postgresql://localhost:5432/postgres";
+    static final String USER = "postgres";
+    static final String PASS = "postgres";
+    static final String QUERY2 = "insert into user_account (id,age,login,fulname,email,gender,) values(1,27,'Edil','tagaibek','edk@','m')";
+    static final String QUERY1 = "delete from users where id=1";
+    static final String QUERY = " select u.login,u.email,ua.account_number,ua.currency from users u\n" +
+            "join users_account ua on u.id = ua.user_id";
+
+    public static void main(String[] args) {
+
+        UsersAccount usersAccount = new UsersAccount();
+        List<String> list = new ArrayList<>(10);
+        Map<String,String>map=new HashMap<>();
+
+
+        try (Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(QUERY)) {
+            //Extract data from result
+            while (rs.next()) {
+                usersAccount.setLogin(rs.getString("login"));
+                usersAccount.setEmail(rs.getString("email"));
+                usersAccount.setAccountNumber(rs.getString("account_number"));
+                usersAccount.setCurrency(rs.getString("currency"));
+                list.add(usersAccount.getEmail() + " " + usersAccount.getAccountNumber() + " " + usersAccount.getCurrency() + " " + usersAccount.getLogin());
+                map.put(usersAccount.getLogin(), usersAccount.getCurrency());
+            } System.out.println(list);
+            System.out.println(map);
+
+//
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/UsersAccount.java
@@ -0,0 +1,48 @@
+public class UsersAccount {
+    private String login;
+    private String email;
+    private String accountNumber;
+    private String currency;
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    @Override
+    public String toString() {
+        return "UsersAccaunt{" +
+                "login='" + login + '\'' +
+                ", email='" + email + '\'' +
+                ", accountNumber='" + accountNumber + '\'' +
+                ", currency='" + currency + '\'' +
+                '}';
+    }
+}
